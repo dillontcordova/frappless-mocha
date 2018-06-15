d18209db86d91e6dad12049f7b1a5101310e6ed4
dillontcordova <dillontcordova@gmail.com>
2018-06-15 13:37:41 -0700

    Update Test_Aws.spec.js

--- Example/Test_Aws.spec.js
+++ Example/Test_Aws.spec.js
@@ -5,7 +5,7 @@
 const FRAPPLESS         = require('../index');
 const AWS_SDK           = require('aws-sdk');
 AWS_SDK.config.region   = 'us-west-2';
-const S3                = new AWS_SDK.S3();
+const S3                = new AWS_SDK.S3({region: 'us-west-2'});
 const DOC_CLIENT        = new AWS_SDK.DynamoDB.DocumentClient();
 
 describe('describe', ( ) => {
@@ -34,7 +34,8 @@ describe('describe', ( ) => {
 
     context( 'context', ( ) => {
 
-        it.EtoE( 'should Three', ( _done ) => {
+        it.EtoE( 'should two', ( _done, payload ) => {
+            //Testing that payload is empty and that it wil create a passable payload to the next EtoE test
             let params = {
                 TableName: 'EXAMPLE_TABLE',
                 Key: {
@@ -42,13 +43,16 @@ describe('describe', ( ) => {
                 }
             };
 
-            DOC_CLIENT.get(params, function( _err, _data ){
-                FRAPPLESS.expect(_err).to.equal(null);
-                _done( _err || null );
-            })
+            setTimeout(() => {
+                DOC_CLIENT.get(params, function( _err, _data ){
+                    FRAPPLESS.expect(_err).to.equal(null);
+                    _done(  null, {some: 'data'} );
+                })
+            },500);
         });
 
-        it.EtoE( 'should Three', ( _done ) => {
+        it.EtoE( 'should Three', ( _done, payload ) => {
+            console.log( payload );
             S3.getObject( {Bucket: 'test'}, function(err, response) {
                 FRAPPLESS.expect(err).to.equal(null);
                 _done( err || null );
