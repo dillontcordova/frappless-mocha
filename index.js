dba2ac2adf3e573ae8a0462504a6e765095c2b69
dillontcordova <dillontcordova@gmail.com>
2018-06-15 13:35:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,23 @@
  * Created by dcordova on 8/16/17.
  */
 
-const GLOBAL        = global || window;
-const MAWS          = require('./lib/mochaAws');
-const NOCK          = require('./lib/mochaNock');
-const REQUEST       = require('./lib/request');
-const CONFIG        = require('./config/mochaConfig');
-const EXPECT        = require('chai').expect;
+const GLOBAL    = global || window;
+const EXPECT    = require('chai').expect;
+const MAWS      = require('./lib/mochaAws');
+const NOCK      = require('./lib/mochaNock');
+const CONFIG    = require('./config/mochaConfig');
 
 
 (function constructor( ){
 
-    const env_Var = process.env.IS_MOCKED;
-    let isMocked = true;
+    const env_Var   = process.env.IS_MOCKED;
+    let isMocked    = true;
     if( env_Var ){
-        const lowerCase = env_Var.toLowerCase();
-        isMocked = lowerCase === 'false' || lowerCase === '0' ? false: !!env_Var;
+        isMocked = !!JSON.parse( env_Var );
     }
 
     CONFIG.setIsMock( isMocked );
-    MAWS.init();
+    MAWS.init       ( );
 
     GLOBAL['expect'] = EXPECT;
     GLOBAL['mochaTest'] = GLOBAL.it;
@@ -28,23 +26,29 @@ const EXPECT        = require('chai').expect;
     if( !isMocked ){
         GLOBAL.it = function(){};
     }
-    GLOBAL.it.EtoE = GLOBAL.mochaTest;
+    GLOBAL.it.EtoE = function(title, fn){
+        GLOBAL.mochaTest( title, (done) => {
+            fn( ( err, data ) => {
+                CONFIG.setPayload( data );
+                done( err );
+            }, CONFIG.getPayload() );
+        });
+    };
 
-    delete GLOBAL.mochaTest;
     delete require.cache[require.resolve('frappless-mocha')];
 })();
 
 
 module.exports = {
-    clean: function(){
+    expect      : EXPECT,
+    aws         : MAWS.mock,
+    nock        : NOCK.nock,
+    setIsMock   : CONFIG.setIsMock,
+    getIsMock   : CONFIG.getIsMock,
+    setPayload  : CONFIG.setPayload,
+    getPayload  : CONFIG.getPayload,
+    clean       : function( ){
         NOCK.clean();
         MAWS.clean();
-    },
-    aws: MAWS.mock,
-    expect: EXPECT,
-    nock: NOCK.nock,
-    request: REQUEST,
-    setIsMock: CONFIG.setIsMock,
-    setPayload: CONFIG.setPayload,  //might be useless now
-    getPayload: CONFIG.getPayload   //might be useless now
+    }
 };
